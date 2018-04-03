60b0289f75f8b74211392b01c56da98edfb02372
Scott <sdmccoy@gmail.com>
2018-04-03 10:36:39 -0700

    random position is set up, need to fix the min

---
+++ main.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const winWidth = window.innerWidth
+const winHeight = window.innerHeight
+
+const getRdmTop = () => {
+  let currentTop = Math.floor(trex.getBoundingClientRect().top)
+  let currentBttm = Math.floor(trex.getBoundingClientRect().bottom)
+  const imgHeight = Math.floor(trex.getBoundingClientRect().height)
+  console.log('cur top', currentTop, 'cur btm', currentBttm);
+  console.log('win height = ', winHeight)
+  console.log('img height = ', imgHeight)  
+  if (currentTop < currentBttm) {
+    let maxMove = winHeight - (currentTop + imgHeight)
+    let minMove = imgHeight - currentTop
+    console.log('move down, max top = ', maxMove);
+    
+    return Math.floor(Math.random() * maxMove)
+  } else {
+    let maxMove = winHeight - (currentBttm + imgHeight)
+    let minMove = imgHeight - currentBttm
+    console.log('move up, top = ', maxMove);
+    return Math.floor(Math.random() * maxMove)
+  }
+}
+
+const getRdmLeft = () => {
+  let currentRight = Math.floor(trex.getBoundingClientRect().right)
+  let currentLeft = Math.floor(trex.getBoundingClientRect().left)
+  console.log('cur right = ', currentRight);
+  console.log('cur left = ', currentLeft);
+  console.log('win width = ', winWidth)
+  const imgWidth = Math.floor(trex.getBoundingClientRect().width)
+  if (currentRight > currentLeft) {
+    let maxMove = winWidth - (currentLeft + imgWidth)
+    return Math.floor(Math.random() * maxMove)
+  } else {
+    let maxMove = winWidth - (currentRight + imgWidth)
+    console.log('max move, left = ', maxMove);
+    return Math.floor(Math.random() * maxMove)
+  }
+}
+
+const moveit = (e) => {
+  
+  let rdmTop = getRdmTop() + 'px'
+  let rdmLeft = getRdmLeft() + 'px'
+
+  console.log('rdm top' , rdmTop, 'rdm right', rdmLeft)
+  e.target.style.top = rdmTop
+  e.target.style.left = rdmLeft
+}
+
+let trex = document.getElementById('trex')
+
+trex.addEventListener('mouseover', moveit)
